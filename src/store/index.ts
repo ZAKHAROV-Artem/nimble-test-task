a0e53941512f08c4895e46b4b45a4531578ce63d
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-07-31 00:02:37 +0200

    feat: rtk setup

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { contactsApi } from "./contacts/contactsApi";
+
+const store = configureStore({
+  reducer: {
+    [contactsApi.reducerPath]: contactsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(contactsApi.middleware),
+});
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

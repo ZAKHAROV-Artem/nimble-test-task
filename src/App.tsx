b83b002f5266e3a961a2aa443dd5bf5eb921b705
ZAKHAROV-Artem <szakharovartem@gmail.com>
2024-07-30 22:50:56 +0200

    feat: added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+import { ContactDetails, Contacts } from "./pages";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Contacts />} />
+        <Route path="/contact/:id" element={<ContactDetails />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
